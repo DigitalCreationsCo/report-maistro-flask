5cdd1c9428316ec5b744e93edd0fbfaf42c5129e
Bryant Mejia <54378250+DigitalCreationsCo@users.noreply.github.com>
2025-07-19 15:15:24 -0700

    Update configuration.py

--- app/configuration.py
+++ app/configuration.py
@@ -1,26 +1,48 @@
+import os
+from dataclasses import dataclass, field, fields
+from typing import Any, Optional
 
-class Config(object):
-	"""
-	Configuration base, for all environments.
-	"""
-	DEBUG = False
-	TESTING = False
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///application.db'
-	BOOTSTRAP_FONTAWESOME = True
-	SECRET_KEY = "MINHACHAVESECRETA"
-	CSRF_ENABLED = True
-	SQLALCHEMY_TRACK_MODIFICATIONS = True
+from langchain_core.runnables import RunnableConfig
+from typing_extensions import Annotated
+from dataclasses import dataclass
 
-	#Get your reCaptche key on: https://www.google.com/recaptcha/admin/create
-	#RECAPTCHA_PUBLIC_KEY = "6LffFNwSAAAAAFcWVy__EnOCsNZcG2fVHFjTBvRP"
-	#RECAPTCHA_PRIVATE_KEY = "6LffFNwSAAAAAO7UURCGI7qQ811SOSZlgU69rvv7"
+DEFAULT_REPORT_STRUCTURE = """The report structure should focus on breaking-down the user-provided topic:
 
-class ProductionConfig(Config):
-	SQLALCHEMY_DATABASE_URI = 'mysql://user@localhost/foo'
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+1. Introduction (no research needed)
+   - Brief overview of the topic area
 
-class DevelopmentConfig(Config):
-	DEBUG = True
+2. Main Body Sections:
+   - Each section should focus on a sub-topic of the user-provided topic
+   - Include any key concepts and definitions
+   - Provide real-world examples or case studies where applicable
+   
+3. Conclusion
+   - Aim for 1 structural element (either a list of table) that distills the main body sections 
+   - Provide a concise summary of the report"""
 
-class TestingConfig(Config):
-	TESTING = True
+@dataclass(kw_only=True)
+class Configuration:
+    """The configurable fields for the chatbot."""
+    report_structure: str = DEFAULT_REPORT_STRUCTURE
+    number_of_queries: int = 2 
+    tavily_topic: str = "general"
+    tavily_days: str = None
+    planner_model: str = "gemini-pro"
+    google_api_key: str = "Google Api Key"
+    writer_model: str = "claude-3-5-sonnet-latest"
+    anthropic_api_key: str = "Anthropic Api Key"
+
+    @classmethod
+    def from_runnable_config(
+        cls, config: Optional[RunnableConfig] = None
+    ) -> "Configuration":
+        """Create a Configuration instance from a RunnableConfig."""
+        configurable = (
+            config["configurable"] if config and "configurable" in config else {}
+        )
+        values: dict[str, Any] = {
+            f.name: os.environ.get(f.name.upper(), configurable.get(f.name))
+            for f in fields(cls)
+            if f.init
+        }
+        return cls(**{k: v for k, v in values.items() if v})
